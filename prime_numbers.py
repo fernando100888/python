6d11c74fee6de2a07d79b6ae4009a6ecf76f3259
fernando <fernando.cr100888@gmail.com>
2022-07-21 10:12:17 -0400

    review

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,18 +1,29 @@
 def prime_number(number):
     counter = 0
 
-    for i in range(1, number + 1):
+    #for i in range(1, number + 1):
+        #if i == 1 or i == number:
+            #continue
+        #if number % i == 0:
+            #counter += 1
+    #if number == 1:
+        #return False
+    #if counter == 0:
+        #return True
+    #else:
+        #return False
+
+    for i in range (1, number + 1):
         if i == 1 or i == number:
             continue
         if number % i == 0:
-            counter += 1
+            counter = counter + 1
     if number == 1:
-        return False
+       return False    
     if counter == 0:
         return True
     else:
         return False
-
 
 def run():
     number = int(input('Write a number: '))
