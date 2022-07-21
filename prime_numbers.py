d2e14eab3c3efdfc119bed3a4af1a224004018d6
fernando <fernando.cr100888@gmail.com>
2022-07-21 09:34:19 -0400

    prime numbers proof

---
+++ prime_numbers.py
@@ -0,0 +1,25 @@
+def prime_number(number):
+    counter = 0
+
+    for i in range(1, number + 1):
+        if i == 1 or i == number:
+            continue
+        if number % i == 0:
+            counter += 1
+    if number == 1:
+        return False
+    if counter == 0:
+        return True
+    else:
+        return False
+
+
+def run():
+    number = int(input('Write a number: '))
+    if prime_number(number) == True:
+        print (str(number), 'is a prime number')
+    else:
+        print (str(number), 'is not a prime number')
+
+if __name__ == '__main__':
+    run()
